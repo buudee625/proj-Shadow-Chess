7b72da9b429b00d4469ed04d5671a0b091ca2076
buudee625 <liu.andrew625@gmail.com>
2022-07-23 18:05:09 -0700

    Accomplish 1st iteration of movement

--- js/main-script.js
+++ js/main-script.js
@@ -1,40 +1,41 @@
-console.log('OK');
-
+const testPrint = function(test){
+    console.log(test);
+};
 // Constants
 //==============================================//
 const pieces = {
         king: {
-            beats: ['queen', 'rook', 'bishop', 'knight', 'catapult'],
+            beats: ['king', 'queen', 'rook', 'bishop', 'knight', 'catapult'],
             flipped: false,
             imgUrl: ''
         },
         queen: {
-            beats: ['rook', 'bishop', 'knight', 'pawn', 'catapult'],
+            beats: ['queen', 'rook', 'bishop', 'knight', 'pawn', 'catapult'],
             flipped: false,
             imgUrl: ''
         },
         rook: {
-            beats: ['bishop', 'knight', 'pawn', 'catapult'],
+            beats: ['rook', 'bishop', 'knight', 'pawn', 'catapult'],
             flipped: false,
             imgUrl: ''
         },
         bishop: {
-            beats: ['knight', 'pawn', 'catapult'],
+            beats: ['bishop', 'knight', 'pawn', 'catapult'],
             flipped: false,
             imgUrl: ''
         },
         knight: {
-            beats: ['pawn', 'catapult'],
+            beats: ['knight', 'pawn', 'catapult'],
             flipped: false,
             imgUrl: ''
         },
         pawn: {
-            beats: ['king'],
+            beats: ['pawn', 'king'],
             flipped: false,
             imgUrl: ''
         },
         catapult: {
-            beats: ['king', 'queen', 'rook', 'bishop', 'knight', 'catapult', 'pawn'],
+            beats: ['king', 'queen', 'rook', 'bishop', 'knight', 'pawn', 'catapult'],
             flipped: false,
             imgUrl: ''
         },
@@ -55,24 +56,28 @@ const bluePlayer = {
     urTurn: false,
 };
 
-
-}
-
 //Cached Elements
 //==============================================//
 
+const tableEl = document.querySelector('.main-board') // the table
+const cellEl = document.querySelectorAll('td') // all cells
+let pieceEl = document.querySelectorAll('button') // all playable pieces
 
-//Event Listeners
-//==============================================//
 
 
 //Functions
 //==============================================//
+
+
 function init(){
     // needs to randomly distribute all pieces on the board faced down
     
     // needs to render the graphic element so the players can see the pieces
 };
+
+function selectPiece(){
+    document.querySelector(e.target)
+}
 
 function move(){
 
@@ -85,3 +90,38 @@ function gridCheck(){
 function canBeat(){
 
 };
+
+//Event Listeners
+//==============================================//
+let elmID;
+let cellID;
+let btnID;
+let elmClass; 
+let elmPiece;
+let selectedCell;
+let selectedPiece; 
+tableEl.addEventListener('click', function(e){
+    elmID = e.target.id;
+    elmClass = e.target.className;
+    if (e.target.classList.contains('piece-btn')) {
+        selectedPiece = document.getElementById(elmID);
+    }
+    selectedCell = document.getElementById(elmID);
+    selectedCell.appendChild(selectedPiece);
+
+    // testPrint(selectedCell);
+    // testPrint(elmClass);
+});
+
+
+
+
+
+
+
+
+// 1. click a piece element, grabs the element by using event listner 
+
+// 2. click a cell, append the grabbed piece to the cell identified by its ID
+
+
