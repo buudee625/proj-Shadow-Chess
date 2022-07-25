4a0a495b705087cc0137a78752b3f1c9ba4fc634
buudee625 <liu.andrew625@gmail.com>
2022-07-25 15:09:33 -0700

    implement drag-drop function

--- js/main-script.js
+++ js/main-script.js
@@ -100,27 +100,75 @@ let elmClass;
 let elmPiece;
 let selectedCell;
 let selectedPiece; 
-tableEl.addEventListener('click', function(e){
+
+const pieceElm = document.querySelector('.piece-btn');
+console.log(pieceEl);
+
+// tableEl.addEventListener('click', function(e){
+//     elmID = e.target.id;
+//     console.log(elmID);
+//     const blueSide = document.querySelector('.blue-side > p');
+//     const redSide = document.querySelector('.red-side > p');
+//     redSide.innerText = elmID;
+//     blueSide.innerText = elmID;
+// });
+
+for (const pieceBtn of document.querySelectorAll('.piece-btn')){
+        pieceBtn.addEventListener('dragstart', function(e){
+            e.dataTransfer.setData('text/plain', e.target.id);
+    });
+}
+
+for (const cells of document.querySelectorAll('.cell')){
+    // change the opacity of the cell when a piece is dragged over it 
+    cells.addEventListener('dragover', function(e){
+        e.preventDefault();
+        cells.style.opacity = '0.5'
+
+    });
+    // the drop function  
+    cells.addEventListener('drop', function(e){
+        e.preventDefault();
+        // when the drop event occurs, defines the id of the element and store it in a variable as a string
+        const droppedPieceId = e.dataTransfer.getData('text/plain');
+        // use this id to select it so it can be appended by using .appendChild()
+        const droppedPiece = document.getElementById(droppedPieceId);
+        cells.appendChild(droppedPiece);
+    });
+    // returns the opacity after the drag event leaves the cell
+    cells.addEventListener('dragleave', function(e){
+        cells.style.opacity = '1';
+    });
+}
+
+
+
+
+
+// Moving logic (may not be used)
+//==============================================//
+// tableEl.addEventListener('click', function(e){
     
-    elmClass = e.target.className;
-    if (e.target.classList.contains('piece-btn')) {
-        elmID = e.target.id;
-        selectedPiece = document.getElementById(elmID);
-        console.log('button selected')
-    } else if (e.target.classList.contains('cell')) {
-        elmID = e.target.id;
-        selectedCell = document.getElementById(elmID);
-        console.log('cell selected')
-    };
-    if (selectedPiece !== undefined && selectedCell !== undefined) {
-        console.log(selectedPiece, '< selected piece');
-        console.log(selectedCell, '< seelected cell');
-        selectedCell.appendChild(selectedPiece);
+//     elmClass = e.target.className;
+//     if (e.target.classList.contains('piece-btn')) {
+//         elmID = e.target.id;
+//         selectedPiece = document.getElementById(elmID);
+//         console.log('button selected')
+//     } else if (e.target.classList.contains('cell')) {
+//         elmID = e.target.id;
+//         selectedCell = document.getElementById(elmID);
+//         console.log('cell selected')
+//     };
+//     if (selectedPiece !== undefined && selectedCell !== undefined) {
+//         console.log(selectedPiece, '< selected piece');
+//         console.log(selectedCell, '< seelected cell');
+//         selectedCell.appendChild(selectedPiece);
         
-    };
+//     };
+
     // testPrint(selectedCell);
     // testPrint(elmClass);
-});
+// });
 
 
 
